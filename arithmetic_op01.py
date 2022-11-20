f9c204054f5b2fd1da9f897532e73e06f9497796
Abdumalik A. Sattorov <+@anorbank.local>
2022-11-20 14:04:48 +0500

    01

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,7 @@
 #Create a variable x and assign it the value of 10.
 x = 10
 #Create a variable y and assign it the value of 5.
-y = 5
+y = 6
 #Create a variable result and assign it the value of x + y.
 
 #Print the value of result.
